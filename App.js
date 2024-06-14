c014d3cba1579a369900076515d1ff499671b04c
Prakshal Shah <prakshalshah78@gmail.com>
2024-06-14 15:24:44 -0400

    01- Intiialization of basics

--- App.js
+++ App.js
@@ -1,40 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-const heading = React.createElement("h1", {
-    id: "head-1"
-}, "hello world from React");
-console.log("heading", heading) // heading is a react element
-// React demands a root tag
-const root = ReactDOM.createRoot(document.getElementById("root"));
-// we have created a root for our react library
-root.render(heading);
-//whatever you want to render inside the root, just pass it
+// React Element => JSX
+const heading = <h1 className="head">Hello MERN</h1>
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+
+// React Functional based component => normal javscript function => it has to return a JSX
+
+const Title = () => {
+    return <h1>This is my title</h1>
+}
+
+const HeadingComp = () => {
+    return (
+        <>
+            {/* way to render react element inside react component */}
+            {heading}
+
+            < h1 className="head" > Hello MERN using React Fun Comp </h1 >
+
+            <Title />
+        </>
+    )
+}
 
 
 
-
-{/* <div id="parent">
-        <div id="child">
-            <h1>I'm heading one</h1>
-        </div>
-</div> */}
-
-// 1. Generate parent div
-// 2. inside of parent's children put div child
-// 3. inside of child's children put h1 
-// 4. inside of h1 put html
-
-
-// const parent = React.createElement("div", {
-//     id: "parent"
-// }, React.createElement("div", {
-//     id: "child"
-// }, React.createElement("h1", {
-//     id: "heading"
-// }, "I'm heading one")));
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(parent);
+// React Component
+root.render(<HeadingComp />);
 
--- Readme.md
+++ Readme.md
@@ -7,12 +7,32 @@
 
 2. What is CDN?
 3. What is crossorigin?
-4. Framework vs Library? 
-
+4. Framework vs Library?
 
 ## Carat vs Tilde
-1. ^ - this will install all minor updates. e.g ^2.12.0 - 2.12.1 is released, it automatically updates to it
-2. ~ - this will install all major updates. e.g ~2.12.0 - 2.12.1 is released, it won't install it, but if 2.13.0 is released it install it automatically. Not Recommended. 
 
+1. ^ - this will install all minor updates. e.g ^2.12.0 - 2.12.1 is released, it automatically updates to it
+2. ~ - this will install all major updates. e.g ~2.12.0 - 2.12.1 is released, it won't install it, but if 2.13.0 is released it install it automatically. Not Recommended.
 
 ## Parcel features
+
+# JSX = Javascript XML / HTML
+
+JSX => Javascript like HTML / XML
+JSX => React.createElement => HTMLElement (render) - parcel / babel
+
+=============
+2 kinds of components
+
+1. Class based components - OLDER
+2. Functional based components - Newer
+
+React Functional based component => normal javscript function => it has to return a JSX
+
+1.  function getRes() {
+
+}
+
+2.  const GetRes = () => {
+    return <h1>hello</h1>
+    }
